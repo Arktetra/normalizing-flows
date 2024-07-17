94514f50c1c1ca99a498424a0f8dcf8af73fc2a1
Arktetra <sameershrestha.sm@gmail.com>
2024-07-17 18:27:04 +0545

    add: channel mask

--- normalizing_flows/utils.py
+++ normalizing_flows/utils.py
@@ -7,14 +7,14 @@ def spatial_checkerboard_mask(w: int, h: int, c: int, invert = False) -> torch.T
 
     Args:
     ----
-        w (int): the width of the input.
-        h (int): the height of the input.
-        c (int): the number of channels in the input.
+        w (int): The width of the input.
+        h (int): The height of the input.
+        c (int): The number of channels in the input.
         invert (bool, optional): Determines whether to invert the mask or not. Defaults to False.
 
     Returns:
     -------
-        torch.Tensor: the mask.
+        torch.Tensor: The mask.
 
     """
     x, y = torch.arange(w, dtype = torch.int32), torch.arange(h, dtype = torch.int32)
@@ -29,4 +29,26 @@ def spatial_checkerboard_mask(w: int, h: int, c: int, invert = False) -> torch.T
 
     return mask
 
+def channel_mask(c: int, invert: bool = False) -> torch.Tensor:
+    """Creates a channel mask.
 
+    Args:
+    ----
+        c (int): The number of channels.
+        invert (bool, Optional): Determines whether to invert the mask or not. Defaults to False.
+
+    Returns:
+    -------
+        torch.Tensor: The mask.
+
+    """
+    mask = torch.cat([
+        torch.ones(c // 2, dtype = torch.float32),
+        torch.zeros(c - c // 2, dtype = torch.float32)
+    ])
+    mask = mask.view(1, c, 1, 1)
+
+    if invert:
+        mask = 1 - mask
+
+    return mask
