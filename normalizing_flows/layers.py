2af2cf07d8b1068ec8383574a612839d2b385787
Arktetra <sameershrestha.sm@gmail.com>
2024-07-17 19:29:31 +0545

    add: GatedConvNet
    GatedConvNet is a module for creating a full
    gated convolutional neural network.

--- normalizing_flows/layers.py
+++ normalizing_flows/layers.py
@@ -259,15 +259,15 @@ class LayerNormChannels(nn.Module):
 
     Args:
     ----
-        c (int): number of channels in the image.
+        in_channels (int): number of channels in the image.
         eps (float): a small constant for numerical stability.
 
     """
 
-    def __init__(self, c: int, eps: float = 1e-5):
+    def __init__(self, in_channels: int, eps: float = 1e-5):
         super().__init__()
-        self.gamma = nn.Parameter(torch.ones(1, c, 1, 1))
-        self.beta = nn.Parameter(torch.zeros(1, c, 1, 1))
+        self.gamma = nn.Parameter(torch.ones(1, in_channels, 1, 1))
+        self.beta = nn.Parameter(torch.zeros(1, in_channels, 1, 1))
         self.eps = eps
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -296,18 +296,27 @@ class GatedConv(nn.Module):
 
     Args:
     ----
-        nn (_type_): _description_
+        in_channels(int): number of input channels.
+        c_hidden (int): number of hidden channels.
 
     """
 
-    def __init__(self, c_in: int, c_hidden: int):
+    def __init__(self, in_channels: int, hidden_channels: int):
         super().__init__()
 
         self.net = nn.Sequential(
             ConcatELU(),
-            nn.Conv2d(in_channels = 2 * c_in, out_channels = c_hidden, kernel_size = 3, padding = 1),
+            nn.Conv2d(
+                in_channels = 2 * in_channels,
+                out_channels = hidden_channels,
+                kernel_size = 3, padding = 1
+            ),
             ConcatELU(),
-            nn.Conv2d(in_channels = 2 * c_hidden, out_channels = 2 * c_in, kernel_size = 1)
+            nn.Conv2d(
+                in_channels = 2 * hidden_channels,
+                out_channels = 2 * in_channels,
+                kernel_size = 1
+            )
         )
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
@@ -326,3 +335,73 @@ class GatedConv(nn.Module):
         val, gate = out.chunk(2, dim = 1)
         return x + val * torch.sigmoid(gate)
 
+class GatedConvNet(nn.Module):
+
+    """A module for gated convolutional neural network.
+
+    Args:
+    ----
+        in_channels (int): number of input channels.
+        hidden_channels (int, Optional): number of hidden channels to model. Defaults to 32.
+        out_channels (int, Optional): number of output channels. Defaults to -1.
+        num_layers (int, Optional): number of layers in the network. Defaults to 3.
+
+    """
+
+    def __init__(
+        self,
+        in_channels: int,
+        hidden_channels: int = 32,
+        out_channels: int = -1,
+        num_layers: int = 3
+    ):
+        super().__init__()
+
+        out_channels = out_channels if out_channels > 0 else 2 * in_channels
+
+        layers = []
+        layers += [
+            nn.Conv2d(
+                in_channels = in_channels,
+                out_channels = hidden_channels,
+                kernel_size = 3, padding = 1
+            )
+        ]
+
+        for idx in range(num_layers):
+            layers += [
+                GatedConv(
+                    in_channels= hidden_channels,
+                    hidden_channels = hidden_channels
+                ),
+                LayerNormChannels(c = hidden_channels)
+            ]
+
+        layers += [
+            ConcatELU(),
+            nn.Conv2d(
+                in_channels = 2 * hidden_channels,
+                out_channels = out_channels,
+                kernel_size = 3,
+                padding = 1
+            )
+        ]
+
+        self.nn = nn.Sequential(*layers)
+
+        self.nn[-1].weight.data.zero_()
+        self.nn[-1].bias.data.zero_()
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        """Performs a forward pass in a GatedConvNet.
+
+        Args:
+        ----
+            x (torch.Tensor): input to the GatedConvNet.
+
+        Returns:
+        -------
+            torch.Tensor: output from the GatedConvNet.
+
+        """
+        return self.nn(x)
