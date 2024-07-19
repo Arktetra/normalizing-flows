2ca3631a89b387f5659c5225265597a0430e3491
Arktetra <sameershrestha.sm@gmail.com>
2024-07-19 19:20:54 +0545

    add: checkpointing for training models

--- normalizing_flows/engine.py
+++ normalizing_flows/engine.py
@@ -6,20 +6,24 @@ from tqdm.auto import tqdm
 
 from typing import Dict, List
 
+
 def train(
     model: torch.nn.Module,
     train_dataloader: torch.utils.data.DataLoader,
     val_dataloader: torch.utils.data.DataLoader,
     optimizer: torch.optim.Optimizer,
-    epochs: int
+    epochs: int,
+    checkpoint: bool = False
 ) -> Dict[str, List]:
     """Trains a model for a number of epochs.
 
     Args:
     ----
         model (torch.nn.Module): The model to train.
-        train_dataloader (torch.utils.data.DataLoader): A dataloader for training the model.
-        val_dataloader (torch.utils.data.DataLoader): A dataloader for validating the model.
+        train_dataloader (torch.utils.data.DataLoader): A dataloader for training the
+        model.
+        val_dataloader (torch.utils.data.DataLoader): A dataloader for validating the
+        model.
         optimizer (torch.optim.Optimizer): An optimizer for minimizing the loss.
         epochs (int): Number of epochs to train the model for.
 
@@ -28,25 +32,23 @@ def train(
         Dict[str, List]: The training and validation loss at each epoch.
 
     """
-    results = {
-        "train_loss": [],
-        "val_loss": []
-    }
+    results = {"train_loss": [], "val_loss": []}
+
+    loss = 1000.0       # A high value of loss for saving the state dict of model that has the minimum loss
 
     tepochs = tqdm(range(epochs))
     for epoch in tepochs:
         tepochs.set_description(f"Epoch {epoch + 1}")
 
-        train_loss = model.training_step(
-            train_dataloader,
-            optimizer
-        )
+        train_loss = model.training_step(train_dataloader, optimizer)
 
-        val_loss = model.validation_step(
-            val_dataloader
-        )
+        if train_loss < loss:
+            loss = train_loss
+            torch.save(model.state_dict, "models/mnist_flow.pth")
 
-        tepochs.set_postfix(train_loss = train_loss.item(), val_loss = val_loss.item())
+        val_loss = model.validation_step(val_dataloader)
+
+        tepochs.set_postfix(train_loss=train_loss.item(), val_loss=val_loss.item())
 
         results["train_loss"].append(train_loss)
         results["val_loss"].append(val_loss)
