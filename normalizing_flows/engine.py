75b26eb0dc3c46f8b2db9b8f03677b4611d66a14
Arktetra <sameershrestha.sm@gmail.com>
2024-07-19 19:32:09 +0545

    fix: checkpointing for training models

--- normalizing_flows/engine.py
+++ normalizing_flows/engine.py
@@ -13,19 +13,20 @@ def train(
     val_dataloader: torch.utils.data.DataLoader,
     optimizer: torch.optim.Optimizer,
     epochs: int,
-    checkpoint: bool = False
+    checkpoint_path: str = None
 ) -> Dict[str, List]:
     """Trains a model for a number of epochs.
 
     Args:
     ----
-        model (torch.nn.Module): The model to train.
-        train_dataloader (torch.utils.data.DataLoader): A dataloader for training the
+        model (torch.nn.Module): the model to train.
+        train_dataloader (torch.utils.data.DataLoader): a dataloader for training the
         model.
-        val_dataloader (torch.utils.data.DataLoader): A dataloader for validating the
+        val_dataloader (torch.utils.data.DataLoader): a dataloader for validating the
         model.
-        optimizer (torch.optim.Optimizer): An optimizer for minimizing the loss.
-        epochs (int): Number of epochs to train the model for.
+        optimizer (torch.optim.Optimizer): an optimizer for minimizing the loss.
+        epochs (int): number of epochs to train the model for.
+        checkpoint_path (str): the path to save the model in.
 
     Returns:
     -------
@@ -42,9 +43,9 @@ def train(
 
         train_loss = model.training_step(train_dataloader, optimizer)
 
-        if train_loss < loss:
+        if train_loss < loss and checkpoint_path is not None:
             loss = train_loss
-            torch.save(model.state_dict, "models/mnist_flow.pth")
+            torch.save(model.state_dict, checkpoint_path)
 
         val_loss = model.validation_step(val_dataloader)
 
